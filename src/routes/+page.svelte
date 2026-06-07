d5532aa18a3c900b41e1f4ed25b2430869014389
kolown2007 <kolown@gmail.com>
2026-06-07 12:46:57 +0800

    changed to dvd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import P1Sonet from '$lib/PH_Collage/Collage.svelte';
+</script>
+
+<script lang="ts">
+  import P1Sonet from '$lib/DVD/setup/p1sonet.svelte';
 </script>
 
 <P1Sonet />
